f1f8d2f0e22540227ccf2f97817c5f07c6f7e6f7
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2020-08-12 15:28:08 -0500

    Initial commit

---
+++ img/Frontend Mentor _ Coding Bootcamp Testimonials Slider_files/app.js
@@ -0,0 +1,12 @@
+// var slide_one=document.querySelectorAll('.left-container, .right-container');
+// var slide_two=document.querySelectorAll('.left-container-two, .right-container-two');
+// var btn_prev=document.querySelector('.prev')
+var btn_next = document.querySelector('.next');
+
+
+
+
+btn_next.addEventListener('click', () => {
+  document.getElementById("left-container-two");
+  
+})
